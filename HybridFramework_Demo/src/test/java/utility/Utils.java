071f964fb897a17531184eb64b2b680d07df263f
ShubhankarRaj <raj8lm@gmail.com>
2014-12-16 06:03:47 -0500

    Util file created, yet to be worked

---
+++ HybridFramework_Demo/src/test/java/utility/Utils.java
@@ -0,0 +1,12 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+
+public class Utils {
+public static WebDriver driver = null;
+public static WebDriver OpenBrowser(int iTestCaseRow)
+{
+	return driver;
+	
+}
+}

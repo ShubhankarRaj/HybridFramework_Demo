cfd8a8f772be27d0f88b2393c7ffad6ec492ee08
ShubhankarRaj <raj8lm@gmail.com>
2014-12-16 15:21:13 +0530

    Commiting the UTILITY folder

--- HybridFramework_Demo/src/test/java/utility/Constant.java
+++ HybridFramework_Demo/src/test/java/utility/Constant.java
@@ -2,5 +2,5 @@ package utility;
 
 public class Constant {
 	public static final String URL="http://www.realtor.com/?source=web";
-	public static final String Path_TestData = "";
+	public static final String Path_TestData = "/src/test/resources/";
 }

c43688860087bff3c548830622623f7716672b82
ShubhankarRaj <raj8lm@gmail.com>
2014-12-16 15:29:10 +0530

    Commiting the files.

--- HybridFramework_Demo/src/test/java/utility/Constant.java
+++ HybridFramework_Demo/src/test/java/utility/Constant.java
@@ -2,5 +2,6 @@ package utility;
 
 public class Constant {
 	public static final String URL="http://www.realtor.com/?source=web";
-	public static final String Path_TestData = "/src/test/resources/";
+	public static final String Path_TestData = "/src/test/resources/testData/TestData.xlsx";
+	
 }

755f22049934790a96d45e6cab5701254da11f72
ShubhankarRaj <raj8lm@gmail.com>
2014-12-16 16:09:27 +0530

    Updating the TestData.xlsx sheet

--- HybridFramework_Demo/src/test/java/utility/Constant.java
+++ HybridFramework_Demo/src/test/java/utility/Constant.java
@@ -2,6 +2,19 @@ package utility;
 
 public class Constant {
 	public static final String URL="http://www.realtor.com/?source=web";
-	public static final String Path_TestData = "/src/test/resources/testData/TestData.xlsx";
+	public static final String Path_TestData = "//src//test//resources//testData//";
+	public static final String File_TestData = "TestData.xlsx";
 	
+	// Test Data sheet columns
+	public static final int Col_TestCaseName = 0;
+	public static final int Col_Browser = 1;
+	public static final int Col_ProductType = 2;
+	public static final int Col_ProductSubType = 3;
+	public static final int Col_MinBudget = 4;
+	public static final int Col_MaxBudget = 5;
+	public static final int Col_Address = 6;
+	public static final int Col_MinBedRoom = 7;
+	public static final int Col_MaxBedRoom = 8;
+	public static final int 
+	public static final int 
 }

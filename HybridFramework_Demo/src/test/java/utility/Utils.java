b01aa340741dd59c035aaaa8da87a1ca28736c25
ShubhankarRaj <raj8lm@gmail.com>
2014-12-21 10:28:34 +0530

    Checking in the Utils.java file

--- HybridFramework_Demo/src/test/java/utility/Utils.java
+++ HybridFramework_Demo/src/test/java/utility/Utils.java
@@ -1,12 +1,37 @@
 package utility;
 
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.util.log.Log;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Utils {
-public static WebDriver driver = null;
-public static WebDriver OpenBrowser(int iTestCaseRow)
-{
-	return driver;
-	
-}
+	public static WebDriver driver = null;
+	public static WebDriver OpenBrowser(int iTestCaseRow){
+		String sBrowserName;
+		sBrowserName = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_Browser);
+		switch(sBrwosername){
+		case mozilla:
+			driver = new FirefoxDriver();
+			Log.info("New Firefox driver initiated");
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			Log.info("Implicit wait applied on the driver for 20 seconds");
+			driver.get(Constant.URL);
+			Log.info("Web Application launched successfully");
+		case iExplorer:
+			driver = new InternetExplorerDriver();
+			Log.info("New Firefox driver initiated");
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			Log.info("Implicit wait applied on the driver for 20 seconds");
+			driver.get(Constant.URL);
+			Log.info("Web Application launched successfully");
+
+		}
+		{
+			return driver;
+
+		}
+	}
 }

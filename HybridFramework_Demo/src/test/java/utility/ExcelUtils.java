6328588a7c87754353206c3c15dc4d1db6c79ed5
ShubhankarRaj <raj8lm@gmail.com>
2014-12-21 20:41:17 +0530

    Committing for ExcelUtil.java

--- HybridFramework_Demo/src/test/java/utility/ExcelUtils.java
+++ HybridFramework_Demo/src/test/java/utility/ExcelUtils.java
@@ -4,11 +4,23 @@ import java.io.File;
 import java.io.FileInputStream;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
 	
+	private static XSSFSheet ExcelWSheet;
+	private static XSSFWorkbook ExcelWBook;
+	private static XSSFCell Cell;
+	private static XSSFRow Row;
+	
+	public static String getCellData(int RowNum, int ColNum){
+		Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+		String CellData = Cell.getStringCellValue();
+		Log.info("Excel Sheet opened!!");
+		return CellData;
+	}
 	public void getfile(int row, int col, String shtName) throws Exception{
 
 		String file = ClassLoader.getSystemResource("testData/"+Constant.File_TestData).getFile();
--- HybridFramework_Demo/src/test/java/utility/Utils.java
+++ HybridFramework_Demo/src/test/java/utility/Utils.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.util.log.Log;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -12,26 +13,35 @@ public class Utils {
 	public static WebDriver OpenBrowser(int iTestCaseRow){
 		String sBrowserName;
 		sBrowserName = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_Browser);
-		switch(sBrwosername){
-		case mozilla:
+		if(sBrowserName.equals("mozilla")){
+
 			driver = new FirefoxDriver();
 			Log.info("New Firefox driver initiated");
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			Log.info("Implicit wait applied on the driver for 20 seconds");
 			driver.get(Constant.URL);
 			Log.info("Web Application launched successfully");
-		case iExplorer:
+		}
+		else if(sBrowserName.equals("iExplorer")){
 			driver = new InternetExplorerDriver();
 			Log.info("New Firefox driver initiated");
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			Log.info("Implicit wait applied on the driver for 20 seconds");
 			driver.get(Constant.URL);
 			Log.info("Web Application launched successfully");
-
 		}
-		{
-			return driver;
-
-		}
+		else if(sBrowserName.equals("chrome")){
+				driver = new ChromeDriver();
+				Log.info("New Chrome driver initiated");
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				Log.info("Implicit wait applied on the Chrome Driver for 20 seconds");
+				driver.get(Constant.URL);
+				Log.info("Web Application launched successfully in CHROME.");
 	}
+
+	{
+		return driver;
+
+	}
+}
 }

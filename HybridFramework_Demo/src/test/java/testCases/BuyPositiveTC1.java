f85c5e08bd6b7ecd3829de4d719d1c6793e427ee
ratnesh <ratnesh_royal@yahoo.com>
2014-12-23 13:23:20 +0530

    TC1Buy commited

---
+++ HybridFramework_Demo/src/test/java/testCases/BuyPositiveTC1.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utility.Log;
+
+public class BuyPositiveTC1 {
+	
+	@BeforeMethod
+	public void beforeMethod(){
+		Log.info("In Before method.");
+		
+	}
+	@Test
+	public void mainTest(){
+		
+	}
+	
+
+}

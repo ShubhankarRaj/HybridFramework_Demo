4676bae973b4fb04944ba6e171f9bca49c43c9fb
ratnesh <ratnesh_royal@yahoo.com>
2014-12-23 13:57:13 +0530

    Commiting full.

--- HybridFramework_Demo/src/test/java/testCases/BuyPositiveTC1.java
+++ HybridFramework_Demo/src/test/java/testCases/BuyPositiveTC1.java
@@ -1,20 +1,28 @@
 package testCases;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import appModules.BuyPage_Action;
+import pageObjects.BaseClass;
+import pageObjects.BuyPage;
 import utility.Log;
+import utility.Utils;
 
 public class BuyPositiveTC1 {
-	
+	public WebDriver driver;
 	@BeforeMethod
 	public void beforeMethod(){
 		Log.info("In Before method.");
-		
+		Log.info("Executing first test - BuyPositiveTC1");
+		driver = Utils.OpenBrowser(1);
+		new BaseClass(driver);	
 	}
 	@Test
-	public void mainTest(){
+	public void mainTest() throws InterruptedException{
+		BuyPage_Action.execute();
 		
 	}
 	

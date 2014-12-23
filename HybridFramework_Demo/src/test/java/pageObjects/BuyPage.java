d648ec08fe45f7e6dd49939a0d043b4ddd11ca32
ratnesh <ratnesh_royal@yahoo.com>
2014-12-23 13:20:34 +0530

    BuyPage commiting

--- HybridFramework_Demo/src/test/java/pageObjects/BuyPage.java
+++ HybridFramework_Demo/src/test/java/pageObjects/BuyPage.java
@@ -4,23 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
+import utility.Log;
 
 public class BuyPage extends BaseClass {
 	//public static WebDriver driver = null;
 	private static WebElement element;
+	public static String sLocation;
+	public static String sSearchCount;
 	 public BuyPage(WebDriver driver){		 
 		 super(driver);
 	 }
-	 public WebElement txt_Buy_Location() throws InterruptedException{
-		 Thread.sleep(10000);
-		// Assert.assertEquals(3, 3);
-		// Thread.sleep(1000);
-		 WebElement element =  driver.findElement(By.id("Location"));		 
-
-		// Thread.sleep(2000);
-	//	 WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
+	 public static WebElement txt_Buy_Location() throws InterruptedException{
+		 element = null;
+		 Log.info("Entering value to location for buy");
+		 Thread.sleep(5000);
+		 WebElement element =  driver.findElement(By.id("Location"));	
 		 return element;
 //		 elem.click();
 //		
@@ -31,26 +32,45 @@ public class BuyPage extends BaseClass {
 //		 WebElement elemtwo = driver.findElement(By.className("i-magnifier-extra-large"));
 //		 elemtwo.click();
 	}
-	public WebElement txt_Buy_FromAmount() throws InterruptedException{
+	public static WebElement txt_Buy_FromAmount() throws InterruptedException{
+		 element = null;
 		 WebElement element =  driver.findElement(By.id("MinimumPrice"));		 
 		 //WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
 		 return element;
 	}		
-	public WebElement txt_Buy_ToAmount() throws InterruptedException{
+	public static WebElement txt_Buy_ToAmount() throws InterruptedException{
+		 element = null;
 		 WebElement element =  driver.findElement(By.id("MaximumPrice"));		 
 		 //WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
 		 return element;
-
 	}
-	public WebElement list_Buy_Beds() throws InterruptedException{
-		 WebElement element =  driver.findElement(By.id("MinimumBed"));		 
+	public static void list_Buy_Beds(String strBed) throws InterruptedException{
+		Select selement = new Select(driver.findElement(By.id("MinimumBed")));
+		selement.selectByVisibleText(strBed);
+	}
+	public static void list_Buy_Baths(String strbath) throws InterruptedException{
+
+		Select selement = new Select(driver.findElement(By.id("MinimumBath")));   	
+   		selement.selectByVisibleText(strbath);	 
+		
+	}	
+	public static WebElement btn_Buy_Search() throws InterruptedException{
+		 element = null;
+		 WebElement element =  driver.findElement(By.className("i-magnifier-extra-large"));		 
 		 //WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
 		 return element;
-
 	}
-	public WebElement list_Buy_Baths() throws InterruptedException{
-		 WebElement element =  driver.findElement(By.id("MinimumBath"));		 
+	public static WebElement txt_Buy_SamePageSearchResult() throws InterruptedException{
+		 element = null;
+		 WebElement element =  driver.findElement(By.className("highlight evtPropCount"));		 
 		 //WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
 		 return element;
 	}	
+	public static WebElement txt_Buy_NextPageSearchResult() throws InterruptedException{
+		 element = null;
+		 WebElement element =  driver.findElement(By.className("property-title"));		 
+		 //WebElement element =  driver.findElement(By.cssSelector(".nav-main-navitems > li:nth-child(1) > a:nth-child(1)"));
+		 return element;
+	}
+	
 }

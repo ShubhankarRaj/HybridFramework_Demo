057d9b14a80e0715c27de6ab7df683cdaf05f3e5
ShubhankarRaj <raj8lm@gmail.com>
2014-12-23 04:28:34 -0500

    Commiting again to test if everyone can see the file MortgageCalculator_action.java

---
+++ HybridFramework_Demo/src/test/java/appModules/MortgageCalculator_Action.java
@@ -0,0 +1,42 @@
+package appModules;
+
+import org.testng.Reporter;
+//import pageObjects.Home_Page;
+import pageObjects.MortgageCalculator;
+import utility.Constant;
+import utility.ExcelUtils;
+import utility.Log;
+
+
+public class MortgageCalculator_Action {
+	public static void Execute(int iTestCaseRow) throws Exception{
+
+//		 Clicking on the My Account link on the Home Page
+//		 Home_Page.lnk_MyAccount().click();
+//		 Log.info("Click action is performed on My Account link" );
+
+		String HomeLocation = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_HomeLocation);
+		MortgageCalculator.EnterHomeLocation().clear();
+		MortgageCalculator.EnterHomeLocation().sendKeys(HomeLocation);
+		Log.info(HomeLocation+" is entered in Location text box" );
+		
+		String HomePrice = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_HomePrice);
+		MortgageCalculator.EnterHomePrice().clear();
+		MortgageCalculator.EnterHomePrice().sendKeys(HomePrice);
+		Log.info(HomePrice+" is entered in Home Price text box" );	
+
+		String DownPayment = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_DownPayment);
+		MortgageCalculator.EnterDownPayment().clear();
+		MortgageCalculator.EnterDownPayment().sendKeys(DownPayment);
+		Log.info(DownPayment+" is entered in DownPayment text box" );
+		
+		String InterestRate = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_MortgageIntRate);
+		MortgageCalculator.EnterInterestRate().clear();
+		MortgageCalculator.EnterInterestRate().sendKeys(InterestRate);
+		Log.info(InterestRate+" is entered in InterestRate text box" );	
+		
+//		Utils.waitForElement(Home_Page.lnk_LogOut());
+//		Reporter.log("SignIn Action is successfully performed");
+
+	}
+}
